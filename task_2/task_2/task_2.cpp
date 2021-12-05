bdc3d848ae2997c0c317752ce2ef3367044c1885
Andrew213 <51080918+Andrew213@users.noreply.github.com>
2021-12-05 21:15:31 +0300

    added task_2

---
+++ task_2/task_2/task_2.cpp
@@ -0,0 +1,32 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    setlocale(LC_ALL, "Ru");
+
+    //R = P / (2π)
+
+    float const pi = 3.14;
+    int circuit;
+
+    cout << "Введите длину окружности: " << flush;
+
+    if (!(cin >> circuit) || (cin.peek() != '\n')) {
+        cout << "Ошибка";
+
+    }
+    else {
+
+        int const rad = circuit / (2 * pi);
+        int const square = pi * pow(rad,2);
+
+        cout << "Площадь окружности: " << square;
+    }
+
+}
+
