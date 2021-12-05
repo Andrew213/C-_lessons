c6dde034fe30ad9b05182bf6b8c818b564d97a76
Andrew213 <51080918+Andrew213@users.noreply.github.com>
2021-12-05 11:04:54 +0300

    task_1

---
+++ task_1/task_1/task_1.cpp
@@ -0,0 +1,64 @@
+﻿ // task_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    cout << "\t\t\t\t\tLiving in America"
+        << "\n"
+        << "\t\t\t\t\tEvery day is an on-going massacre"
+        << "\n"
+        << "\t\t\t\t\tAnother one gets shot down in the streets"
+        << "\n"
+        << "\t\t\t\t\tBut they say it's the land of the free - no!"
+        << "\n"
+        << "\t\t\t\t\tEveryone has a gun, counting bullets like their Haribos"
+        << "\n"
+        << "\t\t\t\t\tSo be careful how you roll"
+        << "\n"
+        << "\t\t\t\t\tNow the playground is the danger zone"
+        << "\n"
+        << "\n"
+        << "\t\t\t\t\tOooh, oooh, oooh, ooooh"
+        << "\n"
+        << "\t\t\t\t\tLiving in America(x2)"
+        << "\n"
+        << "\t\t\t\t\tOooh, oooh, oooh, ooooh"
+        << "\n"
+        << "\t\t\t\t\tLiving in America(x2)"
+        << "\n"
+        << "\n"
+        << "\n"
+        << "\t\t\t\t\t\t\t"
+        << char(169)
+        << " Living in America. SAULT"
+        << "\n"
+        << "\n"
+        << "\n"
+        << "\n"
+        << "\n"
+        << "\n";
+               
+
+
+
+
+
+
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
