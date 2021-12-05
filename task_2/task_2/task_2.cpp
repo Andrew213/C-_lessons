75141269c50a69806e0f2dfbc64e47ca8b410b08
Andrew213 <51080918+Andrew213@users.noreply.github.com>
2021-12-05 12:04:33 +0300

    task_2

---
+++ task_2/task_2/task_2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+
+    setlocale(LC_ALL, "Ru");
+
+    HANDLE Console = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    SetConsoleTextAttribute(Console, FOREGROUND_RED);
+    cout << "\t\t\t\t\t"
+        << "Каждый\n";
+
+    SetConsoleTextAttribute(Console, 6);
+    cout << "\t\t\t\t\t"
+        << "Охотник\n";
+
+    SetConsoleTextAttribute(Console, 14);
+    cout << "\t\t\t\t\t"
+        << "Желает\n";
+
+    SetConsoleTextAttribute(Console, FOREGROUND_GREEN);
+    cout << "\t\t\t\t\t"
+        << "Знать \n";
+
+    SetConsoleTextAttribute(Console, 3);
+    cout << "\t\t\t\t\t"
+        << "Где \n";
+
+    SetConsoleTextAttribute(Console, 1);
+    cout << "\t\t\t\t\t"
+        << "Сидит \n";
+
+    SetConsoleTextAttribute(Console, 5);
+    cout << "\t\t\t\t\t"
+        << "Фазан \n";
+
+        
+
+    cin.get();
+}
