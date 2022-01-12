e66e4b67f90b00d3713467276aa22fb9fd43d58a
Andrew213 <51080918+Andrew213@users.noreply.github.com>
2022-01-12 15:37:50 +0300

    task_8

---
+++ task_function_7/task_function_7/task_function_7.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+void FillArray(int* const arr, int const size) {
+
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand() % 50;
+	}
+}
+
+float getAverage(int* const arr, float const size) {
+	int sum = 0;
+	for (int i = 0; i < size; i++) {
+		sum += arr[i];
+	}
+
+	return sum / size;
+}
+
+int main()
+{
+
+	setlocale(0, "ru");
+
+	int const size = 10;
+	int arr[size]{};
+
+	FillArray(arr, size);
+
+	cout << "Среднее арифметическое равно: " << getAverage(arr, size) << endl;
+
+
+}
+
---
+++ task_function_8/task_function_8/task_function_8.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+void showInfo(int* const arr, float const size) {
+	int null = 0;
+	int pozitiv = 0;
+	int negativ = 0;
+	for (int i = 0; i < size; i++) {
+		if (arr[i] == 0) null += 1;
+		if (arr[i] > 0) pozitiv += 1;
+		if (arr[i] < 0) negativ += 1;
+
+	}
+
+	cout << "Нолей в массиве: " << null << endl;
+	cout << "Положительных чисел в массиве: " << pozitiv << endl;
+	cout << "Отрицательных чисел в массиве: " << negativ << endl;
+}
+
+int main()
+{
+
+	setlocale(0, "ru");
+
+	int const size = 10;
+	int arr[size]{ 0,-2,123,4,-23,1,3,555 };
+
+	showInfo(arr, size);
+
+}
+
