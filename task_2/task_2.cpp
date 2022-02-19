d880e4f34a8de65412eeb527591a7ab304842f68
AND <Andrew213>
2022-02-19 18:27:26 +0300

    cows and bulls game

--- task_2/task_2.cpp
+++ task_2/task_2.cpp
@@ -1,127 +1,85 @@
 #include <iostream>
 using namespace std;
-constexpr int SIZE = 10;
 
-void FillArr(int arr[], int size){
-    for(int i = 0; i < size; i++){
-        arr[i] = rand() % 50 + 1;
-    }
+// Написать игру «Быки и коровы». Программа "загадывает" четырёхзначное число и играющий
+// должен угадать его. После ввода пользователем числа программа сообщает, сколько цифр числа
+// угадано (быки) и сколько цифр угадано и стоит на нужном месте (коровы). После отгадывания
+// числа на экран необходимо вывести количество сделанных пользователем попыток. В
+// программе необходимо использовать рекурсию.
+
+int* decomposeNum(const int hiddenNum){
+    int* hiddenNumsArr = new int[4];
+    
+    hiddenNumsArr[0] = hiddenNum / 1000;
+    hiddenNumsArr[1] = ((hiddenNum % 1000) - (hiddenNum % 100)) / 100;
+    hiddenNumsArr[2] = ((hiddenNum % 100) - hiddenNum % 10) / 10;
+    hiddenNumsArr[3] = hiddenNum % 10;
+
+  
+    return hiddenNumsArr;
 }
 
-void ShowArr(int arr[], int size){
-    for(int i = 0; i < size; i++){
-        cout << arr[i]<<" ";
-    }
+void StartGame(int hiddenArr[], int tryingCount = 1){
 
-    cout<<endl;
-}
+    int tryCount = tryingCount;
+    int userNum = 0;
+    int bulls = 0;
+    int cows = 0;
 
-void findMaxInArr (int arr[], int size){
-    int maxNum = arr[0];
+    cout << "Число было загадано."<<endl;
 
-    for(int i = 0; i < size; i++){
-        if(maxNum < arr[i]) maxNum = arr[i];
-    }
-    cout<<"Max num at Arr One: "<<maxNum<<endl;
-}
+    cout<< "Ваш ход, введите 4-х значное число: ";
+    
+    cin >> userNum;
 
-//##################################
+    int* userArr = decomposeNum(userNum);
 
-void FillArr(int arr[SIZE][SIZE], int size){
-    for(int i = 0; i < size; i++){
-        for(int k = 0; k < size; k++){
-            arr[i][k] = rand() % 50 + 1;
-        }
-    }
-}
-
-void ShowArr(int arr[SIZE][SIZE], int size){
-    for(int i = 0; i < size; i++){
-        for(int k = 0; k < size; k++){
-            cout<<arr[i][k]<<" ";
-        }
-    cout<<endl;
-    }
-
-}
-
-void findMaxInArr (int arr[SIZE][SIZE], int size){
-    int maxNum = arr[0][0];
-    for(int i = 0; i < size; i++){
-        for(int k = 0; k < size; k++){
-            if(maxNum < arr[i][k]) maxNum = arr[i][k];
-        }
-    }
-    cout<<"Max num at Arr One: "<<maxNum<<endl;
-}
-
-//##################################
-
-void FillArr(int arr[SIZE][SIZE][SIZE], int size){
-    for(int i = 0; i < size; i++){
-        for(int k = 0; k < size; k++){
-            for(int j = 0; j < size; j++){
-            arr[i][k][j] = rand() % 150 + 1;
+    for(int i = 0; i < 4; i++){
+        for(int k = 0; k<4; k++){
+            if(hiddenArr[i] == userArr[k]){
+                bulls++;
+                if(i == k){
+                    cows++;
+                }
             }
-        }
+        }        
     }
-}
+    
+    cout<< "bulls " << bulls <<endl;
+    cout<< "cows " << cows <<endl;
 
-void ShowArr(int arr[SIZE][SIZE][SIZE], int size){
-    for(int i = 0; i < size; i++){
-        for(int k = 0; k < size; k++){
-            for(int j = 0; j < size; j++){
-                cout<<arr[i][k][j]<<" ";
-            }
-        }
-    cout<<endl;
+    if(cows == 4){
+        cout << "Поздравляем, вы отгадали число!: " << userNum << endl;
+        cout << "Число попыток: " << tryCount << endl;
+    }else{
+        tryCount++;
+        cout << "Мимо, повторите снова" << endl;
+        StartGame(hiddenArr, tryCount);
     }
 
+    delete[] userArr;
+    // for(int i = 0; i < 4; i++){
+    //     cout<<hiddenArr[i]<< " ";     
+    // }
+    // cout<<endl;
+    // for(int i = 0; i < 4; i++){
+    //     cout<<userArr[i]<< " ";     
+    // }
+
+    // cout<<endl;
 }
-
-void findMaxInArr (int arr[SIZE][SIZE][SIZE], int size){
-    int maxNum = arr[0][0][0];
-
-    cout<<endl;
-
-    for(int i = 0; i < size; i++){
-        for(int k = 0; k < size; k++){
-            for(int j = 0; j < size; j++){
-             if(maxNum < arr[i][k][j]) maxNum = arr[i][k][j];
-            }
-        }
-    }
-    cout<<"Max num at Arr One: "<<maxNum<<endl;
-}
-
-//##################################
-int findMax (int a, int b){
-    int maxNum=0;
-    a > b ? maxNum = a : maxNum = b;   
-          
-    return maxNum;
-}
-
-int findMax (int a, int b, int c){
-    int maxNum=0;
-    int k = findMax(b,c);
-    a >  k ? maxNum = a : maxNum = k;   
-
-    return maxNum;
-}
-
 
 int main(){
-
+    setlocale(LC_ALL, "Ru");
     srand(time(NULL));
 
-    int arr_One[SIZE]{};
-    int arr_Two[SIZE][SIZE]{{},{}};
-    int arr_Three[SIZE][SIZE][SIZE]{{},{},{}};
+    int hiddenNum =  rand()%(9999-1000+1) + 1000;
 
-    // FillArr(arr_Three, SIZE);
-    // ShowArr(arr_Three, SIZE);
-    // findMaxInArr(arr_Three, SIZE);
-    const int maxNum = findMax(2,10,22);
-    cout<< maxNum<<endl;
+    int* hiddenArr = decomposeNum(hiddenNum);
+    
+    StartGame(hiddenArr);
+
+    delete[] hiddenArr;
 }
+
+
--- task_2/tempCodeRunnerFile.cpp
+++ task_2/tempCodeRunnerFile.cpp
@@ -1,22 +1,8 @@
-void FillArr(int arr[], int size){
-    for(int i = 0; i < size; i++){
-        arr[i] = rand() % 50 + 1;
-    }
-}
-
-void ShowArr(int arr[], int size){
-    for(int i = 0; i < size; i++){
-        cout << arr[i]<<" ";
-    }
-
-    cout<<endl;
-}
-
-void findMaxInArr (int arr[], int size){
-    int maxNum = arr[0];
-
-    for(int i = 0; i < size; i++){
-        if(maxNum < arr[i]) maxNum = arr[i];
-    }
-    cout<<"Max num at Arr One: "<<maxNum<<endl;
-}
+  // for(int i = 0; i < 4; i++){
+    //     for(int k = 0; k<4; k++){
+    //         if(hiddenArr[i] = userArr[k]){
+    //             bulls++;
+    //         }
+    //     }        
+    // }
+    
